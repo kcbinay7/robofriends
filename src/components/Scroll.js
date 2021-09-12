a6227404af08113d4b7b4bf86bccbff7f6e3adf8
kcbinay7 <61268175+kcbinay7@users.noreply.github.com>
2021-09-12 15:34:07 +1000

    first commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,20 @@
+
+
+const Scroll= (props)=>{
+
+	return(
+
+		<div style={{overflowY:'scroll', 
+		border: '2px solid black', 
+		height:'800px' }} >
+
+			{props.children}
+
+
+
+		</div>
+
+		);
+}
+
+export default Scroll;
